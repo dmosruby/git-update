8f7d3e939c1237894e3a732e7fcf18fb9841b4e6
David Mosto <david.mosto@hitachivantara.com>
2018-08-15 18:15:24 -0700

    Updated to handle nested git directories

--- git-update.sh
+++ git-update.sh
@@ -4,45 +4,56 @@ FOLDER=$1
 
 cd $FOLDER
 
-DIR=(*)
+DIR=$(find . -name .git -type d -prune)
+
 ERROR=()
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-for i in "${DIR[@]}"
-do
-    cd $i
+git_update() {
     BRANCH=$(git rev-parse --abbrev-ref HEAD)
     if [[ "$BRANCH" == "develop" ]]
     then
         if [[ -n "$(git status --porcelain)" ]]
         then
-            echo "${bold}There are changes in $i${normal}"
-            ERROR+=("${bold}There are changes in $i${normal}")
+            echo "${bold}There are changes in $1${normal}"
+            ERROR+=("${bold}There are changes in $1${normal}")
 
         else
-            echo "${bold}Pulling $i${normal}"
+            echo "${bold}Pulling $1${normal}"
             git pull
         fi
     elif [[ "$BRANCH" == "master" ]]
     then
         if [[ -n "$(git status --porcelain)" ]]
         then
-            echo "${bold}There are changes in $i${normal}"
-            ERROR+=("${bold}There are changes in $i${normal}")
+            echo "${bold}There are changes in $1${normal}"
+            ERROR+=("${bold}There are changes in $1${normal}")
         else
-            echo "${bold}Pulling $i${normal}"
+            echo "${bold}Pulling $1${normal}"
             git pull
         fi
     else
-        echo "${bold}$BRANCH is checked-out in $i${normal}"
-        ERROR+=("${bold}$BRANCH is checked-out in $i${normal}")
+        echo "${bold}$BRANCH is checked-out in $1${normal}"
+        ERROR+=("${bold}$BRANCH is checked-out in $1${normal}")
     fi
-    cd ..
+}
+
+for i in ${DIR[@]}
+do
+    clean_dir=${i%????}
+    cd $clean_dir
+    git_update $clean_dir
+    cd $FOLDER
 done
 
 printf "\n"
-echo "The following branchs could not be updated:"
+
+if [ ! -z "$ERROR" ]
+then
+    echo "The following branchs could not be updated:"
+fi
+
 for i in "${ERROR[@]}"
 do
     echo $i
